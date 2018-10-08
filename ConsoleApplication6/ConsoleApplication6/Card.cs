d112469211db08cbe57dbccbe7a8e0a2a086bf66
ruslankozhushko98 <ruslankozhushko98@gmail.com>
2018-10-08 20:38:46 +0300

    Всё готово к проверке

---
+++ ConsoleApplication6/ConsoleApplication6/Card.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication6
+{
+    static public class Lib
+    {
+        static public Random rand = new Random();
+
+        public static Dictionary<int, string> SuitDictionary = new Dictionary<int, string>
+        {
+            { 0, "Чирва"  },
+            { 1, "Пика"   },
+            { 2, "Буба"   },
+            { 3, "Крести" }
+        };
+
+        public static Dictionary<int, string> RankDictionary = new Dictionary<int, string>
+        {
+            { 0, "6"      },
+            { 1, "7"      },
+            { 2, "8"      },
+            { 3, "9"      },
+            { 4, "10"     },
+            { 5, "Валет"  },
+            { 6, "Дама"   },
+            { 7, "Король" },
+            { 8, "Туз"    }
+        };
+
+        static public Hand getHand(bool[,] deck)
+        {
+            var hand = new Hand();
+
+            int _suit = 0, _rank = 0;
+
+            for (int i = 0; i != 6; i++)
+            {
+                do
+                {
+                    _suit = rand.Next(4);
+                    _rank = rand.Next(9);
+                } while (deck[_suit, _rank]);
+
+                deck[_suit, _rank] = true;
+                hand.cards.Add(new Card(_suit, _rank));
+            }
+
+            return hand;
+        }
+    }
+
+    public class Card
+    {
+        public int suit;
+        public int rank;
+
+        public Card(int _suit, int _rank)
+        {
+            suit = _suit;
+            rank = _rank;
+        }
+    }
+
+    public class Hand
+    {
+        public List<Card> cards = new List<Card>(); 
+        public int weight = 0;
+
+        public void calculateWeight(int trump)
+        {
+            weight = -1;
+
+            foreach (var item in cards)
+            {
+                if (item.suit == trump)
+                {
+                    if (weight < item.rank)
+                    {
+                        weight = item.rank;
+                    }
+                }
+            }
+
+            if (weight == -1)
+            {
+                foreach (var item in cards)
+                {
+                    if (weight < item.rank)
+                    {
+                        weight = item.rank;
+                    }
+
+                    if (weight == 8)
+                    {
+                        break;
+                    }                    
+                }
+            }
+            else
+            {
+                weight += 10;
+            }
+        }
+    }
+}
---
+++ ConsoleApplication6/ConsoleApplication6/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[,] deck = new bool[4,9];
+
+            for (int i = 0; i<=3; i++)
+            {
+                for (int j = 0; j <= 8; j++)
+                {
+                    deck[i, j] = false;
+                }
+            }
+
+            Console.WriteLine("Введите кол-во игроков: ");
+            int amountPlayers = Convert.ToInt32(Console.ReadLine());
+            int trump;
+            
+            List<Hand> hands = new List<Hand>();
+
+            trump = Lib.rand.Next(4);
+
+            Console.WriteLine("Козырь: " + Lib.SuitDictionary[trump]);
+
+            for (int i = 0; i != amountPlayers; i++)
+            {
+                var hand = Lib.getHand(deck);
+
+                hand.calculateWeight(trump);
+                hands.Add(hand);
+            }
+
+            int maxVes = -1;
+            int maxInd = -1;
+
+            for (int ind = 0; ind != hands.Count; ind++)
+            {
+                var _ruka = hands[ind];
+
+                Console.WriteLine("Рука № " + ind);
+
+                string str = "";
+
+                foreach (var item in _ruka.cards)
+                {
+                    str += " " + Lib.SuitDictionary[item.suit] + " " + Lib.RankDictionary[item.rank] + ", ";
+                }
+
+                Console.WriteLine(str);
+
+                if (maxVes < _ruka.weight)
+                {
+                    maxInd = ind;
+                    maxVes = _ruka.weight;
+                } 
+            }
+
+            Console.WriteLine("Самая сильная рука: " + maxInd);
+            Console.ReadKey();
+        }
+    }
+}
